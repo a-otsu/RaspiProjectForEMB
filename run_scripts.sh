34159d01fb337443579a099e84a0d07c21fb2034
a-otsu <a-otsu@g.u-fukui.ac.jp>
2025-05-15 10:07:15 +0900

    update sh

--- run_scripts.sh
+++ run_scripts.sh
@@ -1,29 +1,37 @@
 #!/bin/bash
-if [! -e /home/pi/RaspiProjectForEMB/log.txt ] ;then
-touch /home/pi/RaspiProjectForEMB/log.txt
+
+LOG_FILE="/home/pi/RaspiProjectForEMB/log.txt"
+
+if [ ! -e "$LOG_FILE" ]; then
+    touch "$LOG_FILE"
 fi
 
-echo "start script at $(date)" >> /home/pi/RaspiProjectForEMBlog.txt
-echo "Waiting for network..." >> /home/pi/RaspiProjectForEMB/log.txt
+echo "start script at $(date)" >> "$LOG_FILE"
+echo "Waiting for network..." >> "$LOG_FILE"
 
-#接続前にスクリプトが終了してしまう場合、MAX_RETRIESを増やす。
 MAX_RETRIES=30
 RETRY_COUNT=0
 
-#ネットワーク接続を待機。一定回数の試行しても接続が確認できない場合スクリプトを終了。
+# ネットワーク接続確認
 until ping -c 1 google.com > /dev/null 2>&1; do
     sleep 10
     RETRY_COUNT=$((RETRY_COUNT + 1))
     if [ "$RETRY_COUNT" -ge "$MAX_RETRIES" ]; then
-        echo "Network not available after multiple attempts, exiting..." >> /home/pi/RaspiProjectForEMB/log.txt
+        echo "Network not available after multiple attempts, exiting..." >> "$LOG_FILE"
         exit 1
     fi
 done
-echo "Network is ready at $(date)" >> /home/pi/RaspiProjectForEMB/log.txt
-/usr/bin/python3 /home/pi/RaspiProjectForEMB/send_data.py >> /home/pi/RaspiProjectForEMB/log.txt 2>&1
-sleep 5
-/usr/bin/python3 /home/pi/RaspiProjectForEMB/send_images.py >> /home/pi/RaspiProjectForEMB/log.txt 2>&1
-sleep 5
-/usr/bin/python3 /home/pi/RaspiProjectForEMB/get_board_conf.py >> /home/pi/RaspiProjectForEMB/log.txt 2>&1
 
-echo "script complete at $(date)" >> /home/pi/RaspiProjectForEMB/log.txt
+echo "Network is ready at $(date)" >> "$LOG_FILE"
+
+cd /home/pi/RaspiProjectForEMB
+
+/usr/bin/python3 send_data.py >> "$LOG_FILE" 2>&1 || exit 1
+echo "Data sent at $(date) wait camera" >> "$LOG_FILE"
+sleep 5
+/usr/bin/python3 send_images.py >> "$LOG_FILE" 2>&1
+echo "images.py exit" >> "$LOG_FILE"
+sleep 5
+/usr/bin/python3 get_board_conf.py >> "$LOG_FILE" 2>&1 || exit 1
+
+echo "script complete at $(date)" >> "$LOG_FILE"
